8c3c19c07a4f1485dfdbb70f8735e7380f2657fe
Gikubu <gikubujoseph@gmail.com>
2023-06-06 23:28:59 +0300

    Name and Height

--- app.py
+++ app.py
@@ -3,8 +3,23 @@ import streamlit as st
 
 #Utils
 
+
 # Setup & Config
 
 
 # App
+
+## Header
+st.write("""# My First App
+         This is my first app containing a form
+         """)
+
+## Form
+with st.form("my_form") as form:
+    name = st.text_input(label="Enter your name, please...") 
+    height = st.number_input("Enter your height in centimeters, please...")
+    
+    submit = st.form_submit_button()
+
+print(f"[info] Here is my name : {name}")
 
